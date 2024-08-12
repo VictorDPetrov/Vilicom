503cf78b13c474569d851728cffbdc7917373776
VictorDPetrov <vdpetrov@outlook.com>
2024-08-12 12:37:28 +0300

    Added database, create footer, style change

---
+++ controller/database.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const database = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root123',
+  database: 'vilicom',
+  debug: false,
+  connectionLimit: 200
+});
+
+console.log("Vilicom is Online!");
+module.exports = { database };
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express'),
     app = express(),
-    port = 5330
+    port = 5330,
+    session = require('express-session'),
+    sessionSecret = "sXC2fFFFDbv55sKqGjrKSWWKK!PtcqPT4adsd231w@wWExdaS@#213s",
+    cookieExpires = 1000 * 60 * 60;
 
-const homePage = require('./routes/home'),
+
+const database = require('./controller/database'), 
+    homePage = require('./routes/home'),
     forUs = require('./routes/forUs')
 
 app.use(express.static('routes'));
@@ -10,6 +15,25 @@ app.use(express.static('view'));
 app.use(express.static('controller'));
 
 app.set('view engine', 'ejs');
+
+// Session
+app.use(session({
+    secret: sessionSecret,
+    name: 'vilicomSession',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: cookieExpires,
+        httpOnly: true
+    },
+}));
+
+// Updating cookie time before expiring (60 minutes)
+app.use(function (req, res, next) {
+    req.session._garbage = Date();
+    req.session.touch();
+    next();
+});
 
 app.use('/', homePage);
 app.use('/forUs', forUs);
