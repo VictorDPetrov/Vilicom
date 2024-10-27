180f3ea49c5b0aa9897bd4ae4bec564dd43db6e1
VictorDPetrov <vdpetrov@outlook.com>
2024-10-27 12:50:12 +0200

    Update for several pages. Added button that scrolls to the top of the page, added section buttons for revealing all of the products for certain section. Major updates.:

--- DatabaseFile/Vilicom - SQL.sql
+++ DatabaseFile/Vilicom - SQL.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE vilicom;
 USE vilicom;
 
+DROP table users;
+
 CREATE TABLE users (
 userID INT PRIMARY KEY AUTO_INCREMENT,
 firstName VARCHAR(100),
@@ -8,4 +10,6 @@ secondName VARCHAR(100),
 lastName VARCHAR(100),
 phoneNumber VARCHAR(16),
 email VARCHAR(200),
-)
+username VARCHAR(100),
+pass VARCHAR(100)
+);
